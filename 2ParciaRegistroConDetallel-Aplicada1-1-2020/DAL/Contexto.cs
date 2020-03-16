1378a4aec891b336e46e12c6070859662f6dd43b
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-03-16 16:45:43 -0400

    Add project files.

---
+++ 2ParciaRegistroConDetallel-Aplicada1-1-2020/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2ParciaRegistroConDetallel_Aplicada1_1_2020.DAL
+{
+    public class Contexto : DbContext
+    {
+        //public DbSet<Productos> Producto { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 1, Descripcion = "chocolate", Precio = 100 });
+            //modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 2, Descripcion = "cafe", Precio = 100 });
+
+            //modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 3, Descripcion = "arroz", Precio = 100 });
+        }
+    }
+}
